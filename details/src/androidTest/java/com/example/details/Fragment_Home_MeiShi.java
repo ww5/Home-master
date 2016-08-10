1b07384e688ff6801921bfc32b5cdec44ced90f4
David <532422959@qq.com>
2016-08-10 22:35:04 +0800

    123

---
+++ details/src/androidTest/java/com/example/details/Fragment_Home_MeiShi.java
@@ -0,0 +1,18 @@
+package com.example.details;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hp on 2016/8/9.
+ */
+public class Fragment_Home_MeiShi extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=  inflater.inflate(R.layout.fragment_home_meishi1,null);
+
+        return view;
+    }
+}
